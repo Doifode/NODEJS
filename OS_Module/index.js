84ef5aae2daa94c05b8e5d2740f018778a856ee0
Doifode <yashdada9001@gmail.com>
2023-10-29 12:35:10 +0530

    os module explanation

---
+++ OS_Module/index.js
@@ -0,0 +1,12 @@
+// os module includes the utility properties related to operating system 
+const os = require('os');
+ console.log(os.freemem() );
+ console.log(os.version());
+ console.log(os.arch());
+ console.log(os.release());
+ console.log(os.tmpdir());
+ console.log(os.hostname());
+ console.log(os.machine());
+ console.log(os.homedir());
+
+  // this module includes the all the functions related to the os 
