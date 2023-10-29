b9aace6e1fb87c425bda37f8a152e0e31aeb74b1
Doifode <yashdada9001@gmail.com>
2023-10-29 17:04:44 +0530

    module structure chan ges

---
+++ Modules/HTTP_Server/index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+http.createServer((req, res) => {
+    if(req.url =="/"){
+        res.end("home page")
+   }
+   if(req.url =="/about"){
+res.end("about page")
+   }
+ }).listen(30000, () => {
+    console.log('hiii')
+})
+
