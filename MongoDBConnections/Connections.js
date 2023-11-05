7f68a3282bebe727edd8d3d0dc5eab7ef24ce1df
Doifode <yashdada9001@gmail.com>
2023-11-05 13:46:43 +0530

    CRUD operation  with mongodb code

---
+++ MongoDBConnections/Connections.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/NodeJsUsers').then(() => {
+    console.log('connection successful')
+}).catch((e) => console.log(e));
+/*
+This code is used to create the database from here only
+mongodb://127.0.0.1:27017/YOUR_DATABASE_NAME
+*/
+
+const schema = new mongoose.Schema({
+    name: String,
+    password: String
+});
+/*
+Here you  can  define the value fields of that collection
+collection is nothing but the json object
+from here you can define the formate  of your body/schema. 
+*/
+
+const user = mongoose.model('users', schema);
+/*
+Here you can create the actual collection name and define the formate of that collection{
+    name:string,
+    password:string,
+    phone:number
+} like this
+*/
+
+module.exports = user;
---
+++ MongoDBConnections/server.js
@@ -0,0 +1,63 @@
+const express = require('express') // express import
+const app = express();
+const user = require('./Connections');
+// connection file import
+app.use(express.json())
+// to store the object in the form of json;
+
+app.post('/', async (req, res) => {
+    try {
+        const sendData = new user(req.body);
+        // here we ae storing the requested  data from body to this variable
+        const saveData = await sendData.save();
+        // here we are actually storing the data in 
+        // to database sendData.save() this  is  the data base function
+        res.send(saveData)
+        //this is the response that  we  are  sending to user to know  the user what happen  
+    } catch (error) {
+        console.log(error)
+        res.status(404).send(error)
+    }
+})
+
+app.get("/users", async (req, res) => {
+    try {
+        const getData = await user.find({});
+        res.send(getData)
+    } catch (error) {
+        console.log(error)
+        res.status(404).send(error)
+    }
+})
+app.get("/users/:id", async (req, res) => {
+    try {
+        const _id = req.params.id;
+        const getByID = await user.findById({ _id });
+        res.send(getByID)
+
+    } catch (error) {
+        console.log(error)
+        res.status(404).send(error)
+    }
+})
+app.patch('/userUpdate/:id', async (req, res) => {
+    try {
+        const _id = req.params.id;
+        const updateData = await user.findByIdAndUpdate({ _id }, req.body, { new: true });
+        res.send(updateData)
+    } catch (e) { res.status(404).send(e) }
+})
+
+app.delete('/deleteUser/:id', async (req, res) => {
+    try {
+        const _id = req.params.id;
+        const deletedata = await user.findByIdAndDelete({ _id });
+        res.send(deletedata);
+    } catch (error) {
+        res.status(500).send(error)
+
+    }
+})
+app.listen(2304, () => {
+    console.log('hi its working on  2304 !')
+})
